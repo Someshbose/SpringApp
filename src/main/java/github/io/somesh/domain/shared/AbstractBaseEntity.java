42dd299f22ec1dfd4ae054aa88edde40021481cc
Someshbose <iamsomeshbose@gmail.com>
2020-10-26 18:41:15 +0530

    Auditoraware changes added

--- src/main/java/github/io/somesh/domain/shared/AbstractBaseEntity.java
+++ src/main/java/github/io/somesh/domain/shared/AbstractBaseEntity.java
@@ -2,13 +2,14 @@ package github.io.somesh.domain.shared;
 
 import java.time.Instant;
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Getter;
 
 /**
  * AbstractBaseEntity class
@@ -17,24 +18,25 @@ import lombok.Getter;
  *
  * @param <T>
  */
-@Getter
+
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class AbstractBaseEntity<T> implements Entity<T> {
 
-  @Column(name = "CREATED_BY", /* nullable = false, */length = 240)
+  @Column(name = "CREATED_BY", nullable = false, length = 240)
   @CreatedBy
   private String createdBy;
 
-  @Column(name = "UPDATED_BY", /* ,nullable = false, */length = 240)
+  @Column(name = "UPDATED_BY", nullable = false, length = 240)
   @LastModifiedBy
   private String lastUpdatedBy;
 
-  @Column(name = "CREATION_DATE" /* ,nullable = false */)
+  @Column(name = "CREATION_DATE", nullable = false)
   @CreatedDate
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss''Z", timezone = "UTC")
   private Instant creationDate;
 
-  @Column(name = "UPDATION_DATE"/* ,nullable = false */)
+  @Column(name = "UPDATION_DATE", nullable = false)
   @LastModifiedDate
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss''Z", timezone = "UTC")
   private Instant updationDate;
